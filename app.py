1f5cc040bc93b771f0a9d4f5ffd6a06f33db7a08
SoorenaSahraian <sourenaair@gmail.com>
2014-12-14 01:56:10 -0500

    Fix for the sock block

--- app.py
+++ app.py
@@ -1,18 +1,31 @@
+
+
+
+
 #!flask/bin/python
 from flask import Flask
 from numpy import mat
 from numpy import random
 import time
 import threading
-import TestCamera
-from multiprocessing import Process
+import cv2.cv as cv
+import video_capture
+
+
 
 app = Flask(__name__)
 
 
+capture = cv.CaptureFromCAM(0)
+
 def my_thread():
-    time.sleep(2)
-    TestCamera.run_camera()
+    img = cv.QueryFrame(capture)
+    cv.SaveImage(str(1)+".jpeg", img)  # actually save the image
+    while True:
+        video_capture.save_image(cv,5,capture)
+        break
+
+    cv.DestroyAllWindows()
     return None
 
 
@@ -20,11 +33,13 @@ def my_thread():
 def index():
     thread = threading.Thread(target=my_thread)
     thread.start()
-    # p = Process(target=my_thread())
-    #p.start()
-    #p.join()
-    return "Hello man !"
+    return "Hello"
+
+
+
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=12346, use_reloader=True) # run this on a differen port as sockets keep geting clogged
+    app.run(host='0.0.0.0', debug=True, port=12347, use_reloader=True)
+
+
